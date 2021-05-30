338935b356ed6bc69b1eccafde5be12d94a0ffcf
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-05-29 22:47:14 -0500

    added pseudo code

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+//create an object array for questions/answers/correct answers
+//create a click response when play hits start quiz, the timer starts and decreases by 1 sec
+//create html div elements that contain the question and answers to choose
+//when user clicks the response, let them know if they got the answer right or wrong
+//    -if user selects wrong answer, deduct 10 secs from the timer
+//    -after we validate user answer we then flash the next question on the screen
+// when all questions are answered or time expires, we end the quiz
+// the user then can save their initials and high score which will be displayed on the high score list
+//    -do not let user enter highscore if they have 0 or less points
+// update the high score list to include the new high score.
+//    -order high scores by highest score in local storage
+// when user clicks the high score link to view high scores, retrieve all highscores from localstorage by highest score using an ordered list
+//    -create a button on page that also clears high scores from local storage
